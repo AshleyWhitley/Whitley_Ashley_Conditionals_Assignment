c8e257f0a9f056fb7b8624825497c9768178116b
Ashley Whitley <paintrevelations@gmail.com>
2015-03-18 20:13:11 -0700

    1st major commit
    Includes an average, array, and Boolean using an OR operator.

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,44 @@ SDI Section 01
 Conditionals Assignment
 March 17, 2015
  */
+
+alert("Ashley Loves Video Games!")
+
+//variables to set up story
+
+var personName = "Ashley";
+var topGames = ["Destiny", "Assassin's Creed", "FarCry4"];
+var gameTypes = 3;
+var destinyHrs = 120;
+var acCreedHrs = 350;
+var farCryHrs = 95;
+var average = (destinyHrs + acCreedHrs + farCryHrs)/3;
+var allGames = [17, 13, 89];   //XBOX1, PS4, and XBOX360
+var total = allGames[0] + allGames[1] + allGames[2];
+
+//outputs to console to set up the story using numbers
+
+console.log("My name is" + " " + personName + " " + "and I am known to play video games every time I can.");
+console.log("My favorite games to play are:" + " " + topGames + ".");
+console.log("I have mastered all" +" "+ gameTypes +" "+ "games.");
+console.log("My average amount of hours I play per game is" +" "+ average +" "+ "hours."); //calculation of average
+console.log("I have" +" "+ total +" "+ "games between all three consoles."); //array used to find total of games
+
+//Finding out if I can buy the new game I want with the money I have using conditionals and a Boolean
+
+var newGame = "Battlefield Hardline";
+var cost = 65;
+var myMoney = 40;
+var getPaid = false; //Boolean
+
+//Output of the reason I want a new game
+console.log("I want to buy" +" "+ newGame +" "+ "so I can stream it on Twitch for my followers, but I don't know if I can afford it.");
+
+//OR Operator to decide whether I can buy the game today or if I have to wait till I get paid.
+if(myMoney >= cost || getPaid){
+    console.log("I can now go buy" +" "+newGame+" "+"today.")
+}else{
+    console.log("I have to wait till I get paid to buy the new game.");
+}
+
+
